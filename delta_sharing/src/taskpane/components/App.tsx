39fe7df33b259f25c2a6d675c91d695a832c3957
Will Girten <will.girten@databricks.com>
2022-10-15 16:54:23 -0400

    Initial commit.

---
+++ delta_sharing/src/taskpane/components/App.tsx
@@ -0,0 +1,94 @@
+import * as React from "react";
+import { DefaultButton } from "@fluentui/react";
+import Header from "./Header";
+import HeroList, { HeroListItem } from "./HeroList";
+import Progress from "./Progress";
+
+/* global console, Excel, require  */
+
+export interface AppProps {
+  title: string;
+  isOfficeInitialized: boolean;
+}
+
+export interface AppState {
+  listItems: HeroListItem[];
+}
+
+export default class App extends React.Component<AppProps, AppState> {
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      listItems: [],
+    };
+  }
+
+  componentDidMount() {
+    this.setState({
+      listItems: [
+        {
+          icon: "Ribbon",
+          primaryText: "boston-housing",
+        },
+        {
+          icon: "Ribbon",
+          primaryText: "nyc-taxi",
+        },
+        {
+          icon: "Ribbon",
+          primaryText: "airlines",
+        },
+      ],
+    });
+  }
+
+  click = async () => {
+    try {
+      await Excel.run(async (context) => {
+        /**
+         * Insert your Excel code here
+         */
+        const range = context.workbook.getSelectedRange();
+
+        // Read the range address
+        range.load("address");
+
+        // Update the fill color
+        range.format.fill.color = "yellow";
+
+        await context.sync();
+        console.log(`The range address was ${range.address}.`);
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  render() {
+    const { title, isOfficeInitialized } = this.props;
+
+    if (!isOfficeInitialized) {
+      return (
+        <Progress
+          title={title}
+          logo={require("./../../../assets/logo-filled.png")}
+          message="Please sideload your addin to see app body."
+        />
+      );
+    }
+
+    return (
+      <div className="ms-welcome">
+        <Header logo={require("./../../../assets/logo-filled.png")} title={this.props.title} message="Delta Sharing" />
+        <p className="ms-font-l">Enter the data provider details.</p>
+        <DefaultButton className="ms-welcome__action" iconProps={{ iconName: "ChevronRight" }} onClick={this.click}>
+          Connect to a Data Provider
+        </DefaultButton>
+        <HeroList message="Discover shared datasets" items={this.state.listItems}></HeroList>
+        <DefaultButton className="ms-welcome__action" iconProps={{ iconName: "ChevronRight" }} onClick={this.click}>
+          Insert into Sheet
+        </DefaultButton>
+      </div>
+    );
+  }
+}
